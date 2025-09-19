9b996b6050ca67ca760bdcc5ab148d7e321a1c79
James Meneses <124942205+jamesmeneses17@users.noreply.github.com>
2025-09-18 19:43:49 -0500

    chore(config): agregar configuración de CORS usando variable de entorno FRONTEND_URL

--- src/main.ts
+++ src/main.ts
@@ -35,8 +35,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:5173', 
-      'https://tu-dominio-frontend.com',
+      configService.get<string>('FRONTEND_URL') ?? 'http://localhost:5173',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
